3a7f9b6fa05ec65bf3ddb70d8ba4375581fb11e9
Tatiana <ttt-ttt@bk.ru>
2022-07-03 18:14:38 +0300

    Task13_var1

---
+++ Task13HomeWork/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Работает максимум с 9-значными положительными и отрицательными числами. Как расширить - знаний пока не хватает)
+Console.WriteLine("Введите целое число.");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = Razryadnost(num);
+if (index < 3)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    int third = Math.Abs(num) % (Convert.ToInt32(Math.Pow(10, index - 2)))/(Convert.ToInt32(Math.Pow(10, index - 3)));
+    Console.WriteLine("Третья цифра " + third);
+}
+
+
+int Razryadnost(int number)
+{
+    int count = 1;
+    while (number / 10 != 0)
+    {
+        count++;
+        number = number / 10;
+    }
+    return count;
+}
+
